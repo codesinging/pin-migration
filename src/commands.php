d76b3743ad616669bcd79a63bf525e806a902cb3
codesinging <codesinging@gmail.com>
2019-12-15 15:35:48 +0800

    Add Reset and Refresh commands.

--- src/commands.php
+++ src/commands.php
@@ -7,6 +7,8 @@
 use CodeSinging\PinMigration\Console\Breakpoint;
 use CodeSinging\PinMigration\Console\Create;
 use CodeSinging\PinMigration\Console\Init;
+use CodeSinging\PinMigration\Console\Refresh;
+use CodeSinging\PinMigration\Console\Reset;
 use CodeSinging\PinMigration\Console\Rollback;
 use CodeSinging\PinMigration\Console\Run;
 use CodeSinging\PinMigration\Console\SeedCreate;
@@ -18,6 +20,8 @@ return [
     Breakpoint::COMMAND_NAME => Breakpoint::class,
     Create::COMMAND_NAME => Create::class,
     Init::COMMAND_NAME => Init::class,
+    Refresh::COMMAND_NAME => Refresh::class,
+    Reset::COMMAND_NAME => Reset::class,
     Rollback::COMMAND_NAME => Rollback::class,
     Run::COMMAND_NAME => Run::class,
     SeedCreate::COMMAND_NAME => SeedCreate::class,
